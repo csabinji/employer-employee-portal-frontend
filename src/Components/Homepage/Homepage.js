4dbb774a87219d364f132645f73dee0b9ab6351d
Sabin Chapagain <csabinji@gmail.com>
2023-01-07 19:19:31 +0545

    start

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Homepage.css';
+import { useHistory } from 'react-router-dom';
+import { Routes } from '../../routes';
+
+const Homepage = () => {
+    const history = useHistory();
+
+    console.log(history);
+
+    const handleAdminLogin = () => {
+        history.push(Routes.AdminLogin.path);
+    };
+
+    const handleEmployeeLogin = () => {
+        history.push(`${Routes.Login}/employee`);
+    };
+
+    const handleEmployerLogin = () => {
+        history.push(`${Routes.Login}/employer`);
+    };
+    return (
+        <div className="homepage-container">
+            <h1>Welcome to the Homepage</h1>
+            <p>Please select one of the following options to login:</p>
+            <button className="login-button" onClick={handleAdminLogin}>Admin</button>
+            <button className="login-button" onClick={handleEmployerLogin}>Employer</button>
+            <button className="login-button" onClick={handleEmployeeLogin}>Employee</button>
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+export const Routes = {
+    AdminLogin: { path: "/admin/login" },
+    Login: { path: "/login/:type" },
+}
